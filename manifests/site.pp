f2d365dfbd64b405c2c8587a8f9a1f41fd762ca5
Mathias Hansson <49320893+mathiashansson@users.noreply.github.com>
2019-09-15 15:48:40 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,9 @@ node default {
   file {'/root/README':
     ensure => file,
     content => 'this is a readme',
-    owern => 'root',
+    owner => 'root',
   }
+  file {'/root/README':
+  owner => 'root',
 }
 
